186ac206edef7ffdc599a1729f18b716bfb68cee
ebubekirkarakurt <ebukar75@gmail.com>
2023-12-10 23:06:51 +0300

    demo uploaded

---
+++ src/components/Cart.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const Cart = (props: Props) => {
+  return (
+    <View>
+      <Text>Dropdown2</Text>
+    </View>
+  )
+}
+
+export default Cart
+
+const styles = StyleSheet.create({})
